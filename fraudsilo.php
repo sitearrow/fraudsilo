a65ea675fe054aa49128fa32eb59c83e6c4090cf
KevinGraham-com <72907393+KevinGraham-com@users.noreply.github.com>
2025-07-25 12:24:22 +0700

    Update fraudsilo.php
    Improved error handling in _doFraudCheck

--- fraudsilo.php
+++ fraudsilo.php
@@ -124,232 +124,184 @@ function fraudsilo_getConfigArray()
 
 function fraudsilo_doFraudCheck(array $params, $checkOnly = false)
 {
+    $responseData = array(
+        'ip' => $params['ip'],
+        'hostname' => gethostbyaddr($params['ip']),
+        'errors' => []
+    );
+    $failfraud = false;
 
-$responseData=array('ip' => $params['ip'], 'hostname' => gethostbyaddr($params['ip']));
+    // --- FraudRecord Check ---
+    if (!empty($params["FraudRecordEnable"]) && !empty($params['FraudRecordApiKey'])) {
+        $namehash = frecordhash(strtolower(str_replace(" ", "", trim($params['clientsdetails']['firstname'] . $params['clientsdetails']['lastname']))));
+        $email = $params['clientsdetails']['email'];
 
-if (isset($params["FraudRecordEnable"]) && !empty($params['FraudRecordApiKey'])) {
-
-$namehash = frecordhash(strtolower(str_replace(" ","",trim($params['clientsdetails']['firstname'].$params['clientsdetails']['lastname']))));
-
-$email=$params['clientsdetails']['email'];
-
-// update gmail to remove anything after + and any dots
-if(strpos($email,"@gmail.com") !== false) {
-  $emailtemp = substr($email0,strrpos($email,"@gmail.com"));
-  if(strpos($email,"+") !== false)
-    $emailtemp = substr($email,0,strpos($email,"+"));
-    $email = str_replace(".","",$emailtemp)."@gmail.com";
-}
-if(strpos($email,"@googlemail.com") !== false) {
-  $emailtemp = substr($email0,strrpos($email,"@gmail.com"));
-  if(strpos($email,"+") !== false)
-    $emailtemp = substr($email,0,strpos($email,"+"));
-    $email = str_replace(".","",$emailtemp)."@gmail.com";
-}
+        // Gmail normalization
+        if (strpos($email, "@gmail.com") !== false || strpos($email, "@googlemail.com") !== false) {
+            $emailtemp = $email;
+            if (strpos($emailtemp, "+") !== false) {
+                $emailtemp = substr($emailtemp, 0, strpos($emailtemp, "+"));
+            }
+            $emailtemp = str_replace(".", "", $emailtemp);
+            $email = $emailtemp . "@gmail.com";
+        }
 
         $fields = array(
-                '_api' => $params['FraudRecordApiKey'],
-                '_action' => 'query',
-                'name' => $namehash,
-                'email' => frecordhash($email),
-                'ip' => frecordhash($params['ip']),
+            '_api' => $params['FraudRecordApiKey'],
+            '_action' => 'query',
+            'name' => $namehash,
+            'email' => frecordhash($email),
+            'ip' => frecordhash($params['ip']),
         );
 
-// add telephone if it exists
-if ($params['clientsdetails']['telephoneNumber'] != "") { $fields['phone']=frecordhash($params['clientsdetails']['telephoneNumber']); }
+        if (!empty($params['clientsdetails']['telephoneNumber'])) {
+            $fields['phone'] = frecordhash($params['clientsdetails']['telephoneNumber']);
+        }
 
-// add address if it exists
-$address = $params['clientsdetails']['address1'].$params['clientsdetails']['address2'].$params['clientsdetails']['city'].$params['clientsdetails']['state'];
-$address = strtolower(str_replace(" ","",trim($address)));
-if ($address != "") {
-$fields['address'] = frecordhash($address);
-}
+        $address = strtolower(str_replace(" ", "", trim($params['clientsdetails']['address1'] . $params['clientsdetails']['address2'] . $params['clientsdetails']['city'] . $params['clientsdetails']['state'])));
+        if (!empty($address)) {
+            $fields['address'] = frecordhash($address);
+        }
 
-        //set the url, number of POST vars, POST data
-        $ch=curl_init("https://www.fraudrecord.com/api/");
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$fields);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);
+        $ch = curl_init("https://www.fraudrecord.com/api");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
         $fraudresult = curl_exec($ch);
-        preg_match("~\<report\>([0-9.\-a-f]+)\</report\>~",$fraudresult,$matches);
-        $result_exp = explode("-",$matches[1]);
+        curl_close($ch);
 
-
-    if (curl_errno($ch)) {
-        logActivity("FraudRecord Check - Connection Error: " . curl_error($ch));
-        return array(
-            "error" => array(
+        if (curl_errno($ch) || !$fraudresult) {
+            logActivity("FraudRecord Check - Connection Error: " . curl_error($ch));
+            $responseData['errors'][] = [
                 "title" => "FraudRecord Error",
                 "description" => "Unable to connect to FraudRecord API. Please check your configuration."
-            )
-        );
-    }
-
-    curl_close($ch);
-
-    if(isset($result_exp[3]) && strlen($result_exp[3]) == 16) { } else {
-        logActivity("FraudRecord Check - Invalid Response: " . $fraudresult. " - ".print_r($fields, 1));
-        return array(
-            "error" => array(
+            ];
+        } elseif (!preg_match("~<report>([0-9.\-a-f]+)</report>~", $fraudresult, $matches)) {
+            logActivity("FraudRecord Check - Invalid Response: " . $fraudresult . " - " . print_r($fields, 1));
+            $responseData['errors'][] = [
                 "title" => "FraudRecord Error",
                 "description" => "Received an invalid response from the FraudRecord API."
-            )
-        );
+            ];
+        } else {
+            $result_exp = explode("-", $matches[1]);
+            $FRriskScore = $result_exp[0];
+            if ((int)$FRriskScore >= (int)$params["FraudRecordRejectScore"]) {
+                $failfraud = true;
+            }
+
+            $responseData['fraudrecord'] = true;
+            $responseData['fraudrecord_score'] = $FRriskScore;
+            $responseData['fraudrecord_count'] = $result_exp[1];
+            $responseData['fraudrecord_reliability'] = $result_exp[2];
+            $responseData['fraudrecord_details'] = $result_exp[3];
+        }
     }
 
-    $FRriskScore = $result_exp[0];
+    // --- Kickbox Check ---
+    if (!empty($params["KickboxEnable"]) && !empty($params['KickboxApiKey'])) {
+        $ch = curl_init("https://api.kickbox.com/v2/verify?email=" . urlencode($params['clientsdetails']['email']) . "&apikey=" . $params['KickboxApiKey']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
 
-    if ($FRriskScore >= $params["FraudRecordRejectScore"]) {
-        $failfraud=true;
-    }
-
-$responseData['fraudrecord'] = true;
-$responseData['fraudrecord_score'] = $FRriskScore;
-$responseData['fraudrecord_count'] = $result_exp[1];
-$responseData['fraudrecord_reliability'] = $result_exp[2];
-$responseData['fraudrecord_details'] = $result_exp[3];
-
-}
-
-if (isset($params["KickboxEnable"]) && !empty($params['KickboxApiKey'])) {
-
-$ch = curl_init("https://api.kickbox.com/v2/verify?email=".urlencode($params['clientsdetails']['email'])."&apikey=".$params['KickboxApiKey']);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    if (curl_errno($ch)) {
-        logActivity("Kickbox Check - Connection Error: " . curl_error($ch));
-        return array(
-            "error" => array(
+        if (curl_errno($ch) || !$response || $httpCode !== 200) {
+            logActivity("Kickbox Check - Connection Error: " . curl_error($ch));
+            $responseData['errors'][] = [
                 "title" => "Kickbox Error",
                 "description" => "Unable to connect to Kickbox API. Please check your configuration."
-            )
-	);
+            ];
+        } else {
+            $KickboxData = json_decode($response, 1);
+            if ($KickboxData['success'] === true) {
+                if (!empty($params['KickboxRejectDisposable']) && $KickboxData['disposable']) $failfraud = true;
+                if (!empty($params['KickboxRejectFree']) && $KickboxData['free']) $failfraud = true;
+                if (!empty($params['KickboxRejectRole']) && $KickboxData['role']) $failfraud = true;
+                if (!empty($params['KickboxRejectDidYouMean']) && $KickboxData['did_you_mean']) $failfraud = true;
+                if (!empty($params['KickboxRejectUndeliverable']) && $KickboxData['result'] === "undeliverable") $failfraud = true;
+
+                $responseData['kickbox'] = true;
+                $responseData['kickbox_disposable'] = $KickboxData['disposable'];
+                $responseData['kickbox_free'] = $KickboxData['free'];
+                $responseData['kickbox_role'] = $KickboxData['role'];
+                $responseData['kickbox_did_you_mean'] = $KickboxData['did_you_mean'];
+                $responseData['kickbox_result'] = $KickboxData['result'];
+                $responseData['kickbox_reason'] = $KickboxData['reason'];
+            }
+        }
     }
 
-curl_close($ch);
+    // --- FraudLabs Pro Check ---
+    if (!empty($params["FraudLabsProEnable"]) && !empty($params['FraudLabsProApiKey'])) {
+        $orderdata = [
+            "first_name" => $params["clientsdetails"]["firstname"],
+            "last_name" => $params["clientsdetails"]["lastname"],
+            "bill_country" => $params["clientsdetails"]["country"],
+            "ip" => $params['ip'],
+            "format" => "json",
+            "email" => $params["clientsdetails"]["email"],
+            "user_order_id" => substr($params["order"]["order_number"], 0, 15),
+            "amount" => $params["order"]["amount"],
+            "key" => $params['FraudLabsProApiKey']
+        ];
+        if ($params["clientsdetails"]["address1"]) $orderdata["bill_addr"] = $params["clientsdetails"]["address1"];
+        if ($params["clientsdetails"]["city"]) $orderdata["bill_city"] = $params["clientsdetails"]["city"];
+        if ($params["clientsdetails"]["state"]) $orderdata["bill_state"] = $params["clientsdetails"]["state"];
+        if ($params["clientsdetails"]["postcode"]) $orderdata["bill_zip_code"] = $params["clientsdetails"]["postcode"];
+        if ($params["clientsdetails"]["telephoneNumber"]) $orderdata["user_phone"] = $params["clientsdetails"]["telephoneNumber"];
 
-if ($httpCode === 200) {
-    $KickboxData = json_decode($response, 1);
+        $model = $params["clientsdetails"]["model"];
+        $currencyCode = ($model instanceof WHMCS\User\Client) ? $model->currencyrel->code : $model->client->currencyrel->code;
+        $orderdata['currency'] = $currencyCode;
 
-if ($KickboxData['success'] == true) {
+        $ch = curl_init("https://api.fraudlabspro.com/v2/order/screen");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $orderdata);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $fraudlabsdata = curl_exec($ch);
+        curl_close($ch);
 
-	if ($KickboxData['disposable'] == true && !empty($params['KickboxRejectDisposable'])) {
-        $failfraud=true;
-	}
-        if ($KickboxData['free'] == true && !empty($params['KickboxRejectFree'])) {
-        $failfraud=true;
-        }
-        if ($KickboxData['role'] == true && !empty($params['KickboxRejectRole'])) {
-        $failfraud=true;
-        }
-	if ($KickboxData['did_you_mean'] != false && !empty($params['KickboxRejectDidYouMean'])) {
-        $failfraud=true;
-        }
-	if ($KickboxData['result'] == "undeliverable" && !empty($params['KickboxRejectUndeliverable'])) {
-        $failfraud=true;
-        }
-
-$responseData['kickbox'] = true;
-$responseData['kickbox_disposable'] = $KickboxData['disposable'];
-$responseData['kickbox_free'] = $KickboxData['free'];
-$responseData['kickbox_role'] = $KickboxData['role'];
-$responseData['kickbox_did_you_mean'] = $KickboxData['did_you_mean'];
-$responseData['kickbox_result'] = $KickboxData['result'];
-$responseData['kickbox_reason'] = $KickboxData['reason'];
-
-    }
-}
-
-}
-
-if (isset($params["FraudLabsProEnable"]) && !empty($params['FraudLabsProApiKey'])) {
-
-$orderdata=array(
-"first_name" => $params["clientsdetails"]["firstname"],
-"last_name" => $params["clientsdetails"]["lastname"],
-"bill_country" => $params["clientsdetails"]["country"],
-"ip" => $params['ip'],
-"format" => "json",
-"email" => $params["clientsdetails"]["email"],
-"user_order_id" => substr($params["order"]["order_number"], 0, 15), 
-"amount" => $params["order"]["amount"],
-"key" => $params['FraudLabsProApiKey'],
-);
-if ($params["clientsdetails"]["address1"]) { $orderdata["bill_addr"] = $params["clientsdetails"]["address1"]; }
-if ($params["clientsdetails"]["city"]) { $orderdata["bill_city"] = $params["clientsdetails"]["city"]; }
-if ($params["clientsdetails"]["state"]) { $orderdata["bill_state"] = $params["clientsdetails"]["state"]; }
-if ($params["clientsdetails"]["postcode"]) { $orderdata["bill_zip_code"] = $params["clientsdetails"]["postcode"]; }
-if ($params["clientsdetails"]["telephoneNumber"]) { $orderdata["user_phone"] = $params["clientsdetails"]["telephoneNumber"]; }
-
-    $model = $params["clientsdetails"]["model"];
-    if ($model instanceof WHMCS\User\Client) {
-        $currencyCode = $model->currencyrel->code;
-    } else {
-        $currencyCode = $model->client->currencyrel->code;
-    }
-$orderdata['currency'] = $currencyCode;
-$ch=curl_init("https://api.fraudlabspro.com/v2/order/screen");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $orderdata);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$fraudlabsdata=curl_exec($ch);
-$fraudlabsresult=json_decode($fraudlabsdata, 1);
-    if (curl_errno($ch)) {
-        logActivity("FraudLabs Pro Check - Connection Error: " . curl_error($ch));
-        return array(
-            "error" => array(
+        if (curl_errno($ch) || !$fraudlabsdata) {
+            logActivity("FraudLabs Pro Check - Connection Error: " . curl_error($ch));
+            $responseData['errors'][] = [
                 "title" => "FraudLabs Pro Error",
                 "description" => "Unable to connect to FraudLabs Pro API. Please check your configuration."
-            )
-	);
+            ];
+        } else {
+            $fraudlabsresult = json_decode($fraudlabsdata, 1);
+            if ($fraudlabsresult['user_order_id'] === substr($params["order"]["order_number"], 0, 15)) {
+                if ((int)$fraudlabsresult['fraudlabspro_score'] >= (int)$params["FraudLabsProRejectScore"]) $failfraud = true;
+                if (!empty($params['FraudLabsProRejectProxy']) && $fraudlabsresult['ip_geolocation']['is_proxy']) $failfraud = true;
+                if (!empty($params['FraudLabsProRejectDataCenter']) && in_array($fraudlabsresult['ip_geolocation']['usage_type'], ["Data Center/Web Hosting/Transit", "Content Delivery Network"])) $failfraud = true;
+                if (!empty($params['FraudLabsProRejectIPMismatch']) && !$fraudlabsresult['billing_address']['is_ip_country_match']) $failfraud = true;
+
+                $responseData['fraudlabspro'] = true;
+                $responseData['fraudlabspro_score'] = $fraudlabsresult['fraudlabspro_score'];
+                $responseData['fraudlabspro_status'] = $fraudlabsresult['fraudlabspro_status'];
+                $responseData['fraudlabspro_ipcity'] = $fraudlabsresult['ip_geolocation']['city'];
+                $responseData['fraudlabspro_ipcountry'] = $fraudlabsresult['ip_geolocation']['country_name'];
+                $responseData['fraudlabspro_iplat'] = $fraudlabsresult['ip_geolocation']['latitude'];
+                $responseData['fraudlabspro_iplong'] = $fraudlabsresult['ip_geolocation']['longitude'];
+                $responseData['fraudlabspro_ipowner'] = $fraudlabsresult['ip_geolocation']['isp_name'];
+                $responseData['fraudlabspro_ipdomain'] = $fraudlabsresult['ip_geolocation']['domain'];
+                $responseData['fraudlabspro_iptype'] = $fraudlabsresult['ip_geolocation']['usage_type'];
+                $responseData['fraudlabspro_ipproxy'] = $fraudlabsresult['ip_geolocation']['is_proxy'];
+                $responseData['fraudlabspro_ipdist'] = $fraudlabsresult['billing_address']['ip_distance_in_km'];
+                $responseData['fraudlabspro_ipmatch'] = $fraudlabsresult['billing_address']['is_ip_country_match'];
+                $responseData['fraudlabspro_id'] = $fraudlabsresult['fraudlabspro_id'];
+                $responseData['fraudlabspro_credits'] = $fraudlabsresult['remaining_credits'];
+            }
+        }
     }
 
-curl_close($ch);
-
-if ($fraudlabsresult['user_order_id'] == substr($params["order"]["order_number"], 0, 15)) {
-
-    if ($fraudlabsresult['fraudlabspro_score'] >= $params["FraudLabsProRejectScore"]) {
-        $failfraud=true;
-    }
-    if ($fraudlabsresult['ip_geolocation']['is_proxy'] == true && !empty($params['FraudLabsProRejectProxy'])) {
-        $failfraud=true;
-    }
-    if ($fraudlabsresult['ip_geolocation']['usage_type'] == "Data Center/Web Hosting/Transit" && !empty($params['FraudLabsProRejectDataCenter'])) {
-        $failfraud=true;
-    }
-    if ($fraudlabsresult['ip_geolocation']['usage_type'] == "Content Delivery Network" && !empty($params['FraudLabsProRejectDataCenter'])) {
-        $failfraud=true;
-    }
-    if ($fraudlabsresult['billing_address']['is_ip_country_match'] == false && !empty($params['FraudLabsProRejectIPMismatch'])) {
-        $failfraud=true;
+    if ($failfraud) {
+        $returnData['error'] = [
+            "title" => $params['ErrorTitle'],
+            "description" => $params['ErrorMsg']
+        ];
     }
 
-
-$responseData['fraudlabspro'] = true;
-$responseData['fraudlabspro_score'] = $fraudlabsresult['fraudlabspro_score'];
-$responseData['fraudlabspro_status'] = $fraudlabsresult['fraudlabspro_status'];
-$responseData['fraudlabspro_ipcity'] = $fraudlabsresult['ip_geolocation']['city'];
-$responseData['fraudlabspro_ipcountry'] = $fraudlabsresult['ip_geolocation']['country_name'];
-$responseData['fraudlabspro_iplat'] = $fraudlabsresult['ip_geolocation']['latitude'];
-$responseData['fraudlabspro_iplong'] = $fraudlabsresult['ip_geolocation']['longitude'];
-$responseData['fraudlabspro_ipowner'] = $fraudlabsresult['ip_geolocation']['isp_name'];
-$responseData['fraudlabspro_ipdomain'] = $fraudlabsresult['ip_geolocation']['domain'];
-$responseData['fraudlabspro_iptype'] = $fraudlabsresult['ip_geolocation']['usage_type'][0];
-$responseData['fraudlabspro_ipproxy'] = $fraudlabsresult['ip_geolocation']['is_proxy'];
-$responseData['fraudlabspro_ipdist'] = $fraudlabsresult['billing_address']['ip_distance_in_km'];
-$responseData['fraudlabspro_ipmatch'] = $fraudlabsresult['billing_address']['is_ip_country_match'];
-$responseData['fraudlabspro_id'] = $fraudlabsresult['fraudlabspro_id'];
-$responseData['fraudlabspro_credits'] = $fraudlabsresult['remaining_credits'];
-}
-
-}
-
-    if (isset($failfraud)) {
-        $returnData["error"] = array("title" => $params['ErrorTitle'], "description" => $params['ErrorMsg']);
-    }
-
-   $returnData['data'] = $responseData;
+    $returnData['data'] = json_encode($responseData);
     return $returnData;
 }
 
